5c3813d37e2465395d9269359151ad51e47568f8
aruss <arusslabs@gmail.com>
2013-08-17 23:58:20 +0200

    Added job managment drafts

---
+++ Kooboo.CMS/Kooboo.CMS.Web/Areas/Sites/Controllers/JobManagmentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kooboo.Job;
+
+namespace Kooboo.CMS.Web.Areas.Sites.Controllers
+{
+    public class JobManagmentController : Controller
+    {
+        //
+        // GET: /Sites/ApplicationManagment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Recycle")]
+        public ActionResult RecyclePost()
+        {
+            System.Web.Hosting.HostingEnvironment.InitiateShutdown();
+            return RedirectToAction("Index");
+        }
+    }
+}
