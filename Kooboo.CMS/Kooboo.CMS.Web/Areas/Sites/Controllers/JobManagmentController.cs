1a62e210c8bc9dd63b644e3a70e7617c5046d1a7
aruss <arusslabs@gmail.com>
2013-09-06 11:14:38 +0200

    Fixes

--- Kooboo.CMS/Kooboo.CMS.Web/Areas/Sites/Controllers/JobManagmentController.cs
+++ Kooboo.CMS/Kooboo.CMS.Web/Areas/Sites/Controllers/JobManagmentController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kooboo.CMS.Account.Services;
+using Kooboo.CMS.Content.Query;
+using Kooboo.CMS.Sites.View;
 using Kooboo.Job;
 
 namespace Kooboo.CMS.Web.Areas.Sites.Controllers
@@ -25,5 +29,30 @@ namespace Kooboo.CMS.Web.Areas.Sites.Controllers
             System.Web.Hosting.HostingEnvironment.InitiateShutdown();
             return RedirectToAction("Index");
         }
+
+        public ActionResult Test()
+        {
+            var userName = (System.Web.HttpContext.Current.User.Identity.IsAuthenticated) ? System.Web.HttpContext.Current.User.Identity.Name : string.Empty;
+
+            var folder = ContentHelper.TextFolder("TestEntities");
+            var query = folder.CreateQuery();
+
+            var nvc = new NameValueCollection {
+                {"UserKey", Guid.NewGuid().ToString()},
+                {"Name", "123132"},
+                {"Value", "321321"},
+                {"ActiveFrom", DateTime.Now.ToString()}
+            };
+
+
+            //order.UUID = UUIDGenerator.DefaultGenerator.Generate(null);
+            //nvc.Add("UserKey", Guid.NewGuid().ToString());
+
+            Kooboo.CMS.Content.Services
+                 .ServiceFactory.TextContentManager.Add(folder.Repository, folder, null, null, nvc, null, null, userName);
+
+            return null;
+        }
+
     }
 }
