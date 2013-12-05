bc4723678ba28e353d295073f9fca0f0a8c815bf
aruss <arusslabs@gmail.com>
2013-12-05 13:28:27 +0100

    ?

--- Kooboo.CMS/Kooboo.CMS.SitesProviders/Kooboo.CMS.Sites.Persistence.EntityFramework/HtmlBlockProvider/HtmlBlockEntity.cs
+++ Kooboo.CMS/Kooboo.CMS.SitesProviders/Kooboo.CMS.Sites.Persistence.EntityFramework/HtmlBlockProvider/HtmlBlockEntity.cs
@@ -9,13 +9,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using Kooboo.CMS.Sites.Models;
 using System.ComponentModel.DataAnnotations;
 using Kooboo.CMS.Common.Persistence.Non_Relational;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace Kooboo.CMS.Sites.Persistence.EntityFramework.HtmlBlockProvider
 {
+	class MyClass
+	{
+		public string SKu { get; set; }
+	}
+
     [Table("Kooboo_CMS_Sites_HtmlBlocks")]
     public class HtmlBlockEntity
     {
@@ -24,6 +31,12 @@ namespace Kooboo.CMS.Sites.Persistence.EntityFramework.HtmlBlockProvider
         {
             this.SiteName = siteName;
             this.Name = name;
+
+			IEnumerable<MyClass> classes = new List<MyClass>();
+
+
+		var moreThan1 =	classes.GroupBy(c => c.SKu).Where(x => x.Count() > 1).ToList(); 
+
         }
         public HtmlBlockEntity(Kooboo.CMS.Sites.Models.HtmlBlock htmlBlock)
             : this(htmlBlock.Site.FullName, htmlBlock.Name)
